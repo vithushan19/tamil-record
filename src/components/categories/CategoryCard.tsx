ed376419adc96b35799d2e0545903da711ae7bca
= <ajevan.mahadaya@uoit.net>
2021-11-24 14:51:34 -0500

    feat: add images to category cards
    Add images to category cards

--- src/components/categories/CategoryCard.tsx
+++ src/components/categories/CategoryCard.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 
 type CategoryProps = {
   title: string;
+  image_path: string;
   // TODO add image prop
 };
 
-export const CategoryCard = ({ title }: CategoryProps) => {
+export const CategoryCard = ({ title, image_path }: CategoryProps) => {
   return (
     <div className='bg-white rounded-xl p-4 text-gray-600 hover:bg-yellow-400'>
       {title}
+      <img className='object-contain h-40 w-full' src={image_path}></img>
     </div>
   );
 };
